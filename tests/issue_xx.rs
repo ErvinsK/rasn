73dd803f0297137c65bdc4510b1b3172325b93c8
ErvinsK <ervins.kampans@gmail.com>
2026-01-30 12:29:21 +0200

    Slight update to the tests

--- tests/issue_xx.rs
+++ tests/issue_xx.rs
@@ -3,21 +3,25 @@ use rasn::prelude::*;
 /*
 World-Schema DEFINITIONS AUTOMATIC TAGS ::=
 BEGIN
-    Variable_Octet_String ::= OCTETSTRING (SIZE(3..8))
+    Variable_Octet_String ::= OCTETSTRING (SIZE(3..8), ...)
 END
 */
 
 #[doc = "OCTET STRING (SIZE(3..8))"]
 #[derive(Default, AsnType, Debug, Clone, Decode, Encode, PartialEq, Eq, Hash)]
-#[rasn(delegate, size("3..=8"))]
+#[rasn(delegate, size("3..=8", extensible))]
 pub struct OCSTR(pub OctetString);
-
 
 #[test]
 fn test_aper_alignment_variable_octet_string() {
+    // Test value within base range
     let original = OCSTR(OctetString::from(vec![0xff; 5]));
     let encoded = rasn::aper::encode(&original).expect("encode");
-    println!("Encoded OCSTR: {:x?}", encoded);
+    let decoded: OCSTR = rasn::aper::decode(&encoded).expect("decode");
+    assert_eq!(original, decoded); 
+    // Test extended value
+    let original = OCSTR(OctetString::from(vec![0xff; 10]));
+    let encoded = rasn::aper::encode(&original).expect("encode");
     let decoded: OCSTR = rasn::aper::decode(&encoded).expect("decode");
     assert_eq!(original, decoded);
 }
