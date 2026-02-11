3caaa09ccaf3dd227a71aa0400e5d2495eca3a72
ErvinsK <ervins.kampans@gmail.com>
2026-02-11 18:26:16 +0200

    Still trouble shooting CHOICE encoding in APER

--- tests/issue_xxxxx.rs
+++ tests/issue_xxxxx.rs
@@ -22,32 +22,29 @@ pub struct A (pub FixedOctetString<2>);
 pub struct B (pub FixedOctetString<4>);
 
 #[doc = "S"]
-#[derive(Default, AsnType, Debug, Clone, Decode, Encode, PartialEq, Eq, Hash)]
-#[rasn(automatic_tags, identifier = "S")]
+#[derive(AsnType, Debug, Clone, Decode, Encode, PartialEq, Eq, Hash)]
+#[rasn(choice, automatic_tags, identifier = "Choice")]
 #[non_exhaustive]
-pub struct S {
+pub enum SChoice {
     #[rasn(identifier = "A")]
-    pub a: A,
+    First(B),
     #[rasn(identifier = "B")]
-    pub b: Option<Integer>,
+    Second(B),
+    #[rasn(extension_addition, identifier = "C")]
+    Third(B),
 }
 
 #[test]
-fn test_aper_alignment_sequence_a() {
-    let value: u16 = 0xffffu16;
-    let mut bits = FixedBitString::<16usize>::default();
-    for i in 0..16 {
-        if (value & (1 << (15 - i))) != 0 {
-            bits.set(i, true);
-        }
-    }
-    let original = S {
-        a: A(bits),
-        b: Some(42.into()),
-    };
+fn test_aper_alignment_choice() {
+    let octets = B(FixedOctetString::from([0xff, 0xff, 0xff, 0xff]));
+    let encoded = rasn::aper::encode(&octets).expect("encode");
+    println!("encoded: {:02X?}", encoded);
+    let decoded: B = rasn::aper::decode(&encoded).expect("decode");
+    assert_eq!(octets, decoded);
+    let original = SChoice::First(B(FixedOctetString::from([0xff, 0xff, 0xff, 0xff])));
     let encoded = rasn::aper::encode(&original).expect("encode");
     println!("encoded: {:02X?}", encoded);
-    let decoded: S = rasn::aper::decode(&encoded).expect("decode");
+    let decoded: SChoice = rasn::aper::decode(&encoded).expect("decode");
     assert_eq!(original, decoded);
 }
 
